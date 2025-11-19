7e067a4b419a582196b19a0bf2aafd88aef2c2b4
Ivan <ivan.gml097@gmail.com>
2025-11-19 09:38:52 +0800

    Update puppeteer to use puppeteer-core

--- cron.js
+++ cron.js
@@ -1,16 +1,20 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 async function runCron() {
     console.log("Running cron at " + new Date());
+
     const browser = await puppeteer.launch({
         headless: true,
+        executablePath: '/usr/bin/google-chrome-stable', // Render system Chrome
         args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
+
     const page = await browser.newPage();
-    await page.goto('https://itrack-asynch.gt.tc/scripts/cron_update_activity_status.php?cron_key=kt12s9FJ82ksQ20x', {
-        waitUntil: 'networkidle2',
-        timeout: 0
-    });
+    await page.goto(
+        'https://itrack-asynch.gt.tc/scripts/cron_update_activity_status.php?cron_key=kt12s9FJ82ksQ20x',
+        { waitUntil: 'networkidle2', timeout: 0 }
+    );
+
     console.log("Cron finished at " + new Date());
     await browser.close();
 }
